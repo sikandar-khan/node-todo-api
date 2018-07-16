ab0b6f084fd52ad2100590d60117966d4c6293c2
Sikandar <ferary_zf@yahoo.com>
2018-07-16 10:37:38 +0300

    configuring auth

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,60 @@
 const mongoose = require('mongoose');
-let User = mongoose.model('User', {
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
+
+const UserSechema = new mongoose.Schema({
     email: {
         type: String,
         require: true,
         minlength: 1,
-        trim: true
-    }
+        trim: true,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            require: true
+        },
+        token: {
+            type: String,
+            require: true
+        }
+    }]
 });
+
+UserSechema.methods.toJSON = function () {
+    let user = this;
+    let userObject = user.toObject();
+    return _.pick(userObject, ['_id', 'email']);
+}
+UserSechema.methods.generateAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+    let token = jwt.sign({
+        _id: user._id.toHexString(),
+        access
+    }, 'abc123').toString();
+    user.tokens.push({
+        access,
+        token
+    });
+    return user.save().then(() => {
+        console.log(token);
+        return token;
+    });
+}
+
+let User = mongoose.model('User', UserSechema);
 module.exports = {
     User
 }
--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const _ = require('lodash');
 
 const {
     mongoose
@@ -11,12 +12,48 @@ const {
     User
 } = require('./models/user');
 
+const port = process.env.PORT || 3000;
 let app = express();
-app.use(bodyParser.json());
-app.post('/todos', (req, res) => {
-    let todo = new Todo({
-        text: req.body.text
-    });
-    todo.save().then((data) => res.send(data), (error) => res.status(400).send(error));
-})
-app.listen(3000, () => console.log('Started on port 3000'));
+app.use(bodyParser.json())
+    .post('/todos', (req, res) => {
+        let todo = new Todo({
+            text: req.body.text
+        });
+        todo.save()
+            .then((data) => res.send(data), (error) => res.status(400).send(error));
+    })
+    .post('/users', (req, res) => {
+        var user = new User(_.pick(req.body, ['email', 'password']));
+        user.save()
+            .then(() => {
+                return user.generateAuthToken();
+                // res.send(data);
+            })
+            .then((token) => {
+                res.header('x-auth', token).send(user);
+            })
+            .catch((error) => {
+                res.status(400).send(error);
+            });
+    })
+    .get('/todos', (req, res) => {
+        Todo.find().then((todos) => {
+            res.send({
+                todos
+            });
+        }, (error) => {
+            res.status(400).send(error);
+        });
+    })
+    .get('/todos/:id', (res, req) => {
+        Todo.findById(res.params.id).then((data) => {
+            req.send(data);
+        }, (error) => {
+            res.status(400).send(error);
+        });
+    })
+    .listen(port, () => console.log(`Started on port ${port}`));
+
+module.exports = {
+    app
+}
